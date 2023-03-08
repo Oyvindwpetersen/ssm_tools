f23ac323c7346e3ecfab6c18535bb6e76105fa0e
Øyvind <oyvind.w.petersen@ntnu.no>
2023-03-08 21:42:51 +0100

    tidy

--- utils/MC_freqaxis.m
+++ utils/MC_freqaxis.m
@@ -14,30 +14,31 @@ function [domegasim,omega_max]=MC_freqaxis(dt_target,T_target)
 
 domegasim=1e-3;
 omega_max=1;
-
 T=2*pi/domegasim;
 
 if T<T_target
-domegasim=2*pi/T_target;
+    domegasim=2*pi/T_target;
 end
 
 converged=false;
 while converged==false
-
-	omegaaxissim=domegasim:domegasim:omega_max;
-	NFFT=2^nextpow2(2*length(omegaaxissim));
     
-% 	t=linspace(0,2*pi/domegasim,NFFT);
-% 	dt=diff(t(1:2));
+    % 	omegaaxissim=domegasim:domegasim:omega_max;
+    
+    N_omega=ceil(omega_max/domegasim);
+    NFFT=2^nextpow2(2*N_omega);
+    
+    % 	t=linspace(0,2*pi/domegasim,NFFT);
+    % 	dt=diff(t(1:2));
     
     dt=(2*pi/domegasim)/(NFFT-1);
-
-	if dt>dt_target
-	omega_max=omega_max+0.1;
-	else
-	converged=true;
-	end
-
+    
+    if dt>dt_target
+        omega_max=omega_max+0.1;
+    else
+        converged=true;
+    end
+    
 end
 
 
