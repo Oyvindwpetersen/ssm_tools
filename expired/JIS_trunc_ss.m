f6d2aaf54ad6a660befad671e8c7ed8607ebd1ec
Øyvind <oyvind.w.petersen@ntnu.no>
2023-06-11 08:22:29 +0200

    tidy

--- expired/JIS_trunc_ss.m
+++
@@ -1,6 +0,0 @@
-function [x_filt p_filt P_ss Pp_ss M_ss L_ss]=JIS_trunc_ss(A,B,G,J,y,x0,R,Q,S,P01,varargin)
-%% Joint input and state estimation for linear system
-%
-error('Use JIS_ss.m instead');
-
-
