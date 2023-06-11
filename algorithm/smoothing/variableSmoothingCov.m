a5d2f8b7ffc3bb71c68925b572b8b5d7ba1b508f
Øyvind <oyvind.w.petersen@ntnu.no>
2023-06-11 08:30:53 +0200

    syntax

--- algorithm/smoothing/variableSmoothingCov.m
+++ algorithm/smoothing/variableSmoothingCov.m
@@ -2,50 +2,50 @@ function [Px Pp Pd] = variableSmoothingCov(A,B,G,J,y,R,Q,S,x0,P01,phi,omega,gamm
 
 %%
 
-pi=inputParser;
-addParameter(pi,'force','disc',@ischar)
+% pi=inputParser;
+% addParameter(pi,'force','disc',@ischar)
 
-parse(pi,varargin{:});
-forcetype = pi.Results.force;
+% parse(pi,varargin{:});
+% forcetype = pi.Results.force;
 
-forcetype_check=[ strcmpi(forcetype,'disc') strcmpi(forcetype,'discrete') strcmpi(forcetype,'modal')];
-if sum(forcetype_check)==0
-    error();
-end
+% forcetype_check=[ strcmpi(forcetype,'disc') strcmpi(forcetype,'discrete') strcmpi(forcetype,'modal')];
+% if sum(forcetype_check)==0
+    % error();
+% end
 
 
-%%
-[Sde,Sae,~]= generalSelection(d_ref,a_ref,{},doflabel);
+%
+% [Sde,Sae,~]= generalSelection(d_ref,a_ref,{},doflabel);
 
-if strcmpi(forcetype,'disc') | strcmpi(forcetype,'discrete');
-    [~,~,Ge,Je]=statespaceModel(phi,omega,gamma,Sae,Sde,Sp,0);
-elseif strcmpi(forcetype,'modal')
-    [~,~,Ge,Je]=statespaceModelModalForce(phi,omega,gamma,Sae,Sde,Sp,0);
-end
+% if strcmpi(forcetype,'disc') | strcmpi(forcetype,'discrete');
+    % [~,~,Ge,Je]=statespaceModel(phi,omega,gamma,Sae,Sde,Sp,0);
+% elseif strcmpi(forcetype,'modal')
+    % [~,~,Ge,Je]=statespaceModelModalForce(phi,omega,gamma,Sae,Sde,Sp,0);
+% end
 
-for k=1:length(L_list)
+% for k=1:length(L_list)
     
-[~,~,P_x_ss{k} P_p_ss{k} P_xp_ss{k} P_px_ss{k}]=JIS_smooth(A,B,G,J,y,R,Q,S,x0,P01,L_list(k));
+% [~,~,P_x_ss{k} P_p_ss{k} P_xp_ss{k} P_px_ss{k}]=JIS_smooth(A,B,G,J,y,R,Q,S,x0,P01,L_list(k));
 
-P_de{k}=[Ge Je]*[P_x_ss{k} P_xp_ss{k} ; P_px_ss{k} P_p_ss{k}]*[Ge.' ; Je.'];
+% P_de{k}=[Ge Je]*[P_x_ss{k} P_xp_ss{k} ; P_px_ss{k} P_p_ss{k}]*[Ge.' ; Je.'];
 
-Px(:,k)=diag(P_x_ss{k});
-Pp(:,k)=diag(P_p_ss{k});
-Pd(:,k)=diag(P_de{k});
+% Px(:,k)=diag(P_x_ss{k});
+% Pp(:,k)=diag(P_p_ss{k});
+% Pd(:,k)=diag(P_de{k});
 
-end
+% end
 
 
-return
-%%
+% return
+%
 
-close all;
-figure(); hold on; grid on;
-plot(Pp'); set(gca,'YScale','log');
+% close all;
+% figure(); hold on; grid on;
+% plot(Pp'); set(gca,'YScale','log');
 
-figure(); hold on; grid on;
-plot(Px'); set(gca,'YScale','log');
+% figure(); hold on; grid on;
+% plot(Px'); set(gca,'YScale','log');
 
-figure(); hold on; grid on;
-plot(Pd'); set(gca,'YScale','log');
+% figure(); hold on; grid on;
+% plot(Pd'); set(gca,'YScale','log');
 
