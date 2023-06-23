ed6d9a3443f2bc05b2a508eeb84e86024c348a9d
Øyvind <oyvind.w.petersen@ntnu.no>
2023-06-23 21:01:09 +0200

    renamed, syntax

--- utils/ImportBeam.m
+++ utils/ImportBeam.m
@@ -1,4 +1,4 @@
-function mod=ImportBeam(L,E,I,m,N_el,varargin)
+function mod=importbeam(L,E,I,m,N_el,varargin)
 
 %%
 
@@ -15,57 +15,42 @@ p=inputParser;
 p.KeepUnmatched=true;
 
 addParameter(p,'type','simplysupport',@ischar) %springsupport
-addParameter(p,'Nm',[],@isnumeric) %springsupport
-addParameter(p,'k0',50,@isnumeric)
-addParameter(p,'k_end',500,@isnumeric)
+addParameter(p,'nm',[],@isnumeric) %springsupport
+addParameter(p,'k_end',0,@isnumeric)
 parse(p,varargin{:})
 
 type=p.Results.type;
-Nm=p.Results.Nm;
-k0=p.Results.k0;
+nm=p.Results.nm;
 k_end=p.Results.k_end;
 
 %%
 
 if strcmpi(type,'simplysupport')
-    [Kg,Mg,f,phi,phi_full,K,M,K_red,M_red,S_red]=SimplySupportModal(L,E,I,m,N_el);
+    bc='support';
 elseif strcmpi(type,'springsupport')
-    [Kg,Mg,f,phi,phi_full,K,M,K_red,M_red,S_red]=SpringSupportModal(L,E,I,m,N_el,Nm,k0,k_end);
+    bc='spring';
 end
 
 mod=struct();
 
-mod.Kg=Kg;
-mod.Mg=Mg;
-mod.f=f;
-mod.phi_red=phi;
-mod.phi=phi_full;
-mod.K=K;
-mod.M=M;
-mod.K_red=K_red;
-mod.M_red=M_red;
-mod.S_red=S_red;
+[mod.Kg,mod.Mg,mod.f,mod.phi,mod.K,mod.M,mod.phi_red,mod.K_red,mod.M_red,mod.S_red,mod.S_red_inv,mod.nodecoord,mod.doflabel]= ...
+simplysupportbeam(L,E,I,m,N_el,'bc',bc,'k_end',k_end);
 
 mod.Omega=diag(mod.f)*2*pi;
 
-dofs={'U' 'UR'};
-number=1:(N_el+1);
-
-mod.doflabel=DofLabel(dofs,number);
-
-mod.nm=Nm;
+mod.nm=nm;
 
 %% Select limited modes
 
-if ~isempty(Nm)
+if ~isempty(nm)
     
-    range_keep=[1:Nm];
+    range_keep=[1:nm];
     
     mod.Kg=mod.Kg(range_keep,range_keep);
     mod.Mg=mod.Mg(range_keep,range_keep);
     mod.Omega=mod.Omega(range_keep,range_keep);
-    mod.f=f(range_keep);
-    mod.phi_red=phi(:,range_keep);
-    mod.phi=phi_full(:,range_keep);
+    mod.f=mod.f(range_keep);
+    mod.phi_red=mod.phi_red(:,range_keep);
+    mod.phi=mod.phi(:,range_keep);
 
 end
