295302b2a1669160d66c9faca8eff1e68255b6db
Øyvind <oyvind.w.petersen@ntnu.no>
2023-07-10 18:20:46 +0200

    psd wn (some checks remain)

---
+++ test_and_verification/Verify_psd_wn.m
@@ -0,0 +1,25 @@
+%% Test PSD of WN
+
+clc
+clear all
+close all
+
+dt=0.02
+
+t=[0:dt:100000];
+w_axis=[0:0.001:1/dt/2]*2*pi;
+
+sigma_wc_squared=8^2
+
+sigma_wd_squared=sigma_wc_squared*dt;
+
+w_sim=randn(size(t))*sigma_wd_squared.^0.5;
+
+[S_welch,w_welch]=estimateSpectrumWelch(w_sim,1/dt,'Nwelch',1000,'unit','rad');
+
+S_test(1,1,1:length(w_axis))=sigma_wc_squared/(2*pi);
+
+plotpsd(w_welch,S_welch,w_axis,S_test*2*dt^2,'xlim',[0 500]); 
+
+% Still not quite sure why dt^2 is needed here, but ok
+% Probably related to energy within Nyquist range
