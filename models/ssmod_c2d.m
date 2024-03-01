24bf19165015bd480f8b218ec17e1c2d2a17bb2a
Øyvind <oyvind.w.petersen@ntnu.no>
2024-03-01 12:57:24 +0100

    tidy

--- models/ssmod_c2d.m
+++ models/ssmod_c2d.m
@@ -30,25 +30,25 @@ B=[];
 F=[];
 
 if ~isempty(Ac) & fast==true
-A=expmnorm(dt*Ac);
+    A=expmnorm(dt*Ac);
 elseif ~isempty(Ac) & fast==false
-A=expm(dt*Ac);
+    A=expm(dt*Ac);
 end
 
 if ~isempty(Bc)
-Ac_inv=eye(size(Ac)) / Ac;
-B=[A-eye(size(A))]*(Ac_inv*Bc);
-F=Ac_inv*(B-Bc*dt)*dt^-1;
+    Ac_inv=eye(size(Ac)) / Ac;
+    B=[A-eye(size(A))]*(Ac_inv*Bc);
+    F=Ac_inv*(B-Bc*dt)*dt^-1;
 end
 
 C=[];
 D=[];
 
 if ~isempty(Cc)
-C=Cc;
+    C=Cc;
 end
 
 if ~isempty(Dc)
-D=Dc;
+    D=Dc;
 end
 
