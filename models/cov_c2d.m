20ec3afd31a4aafd9b4c71eace03dc3efc70a499
Øyvind <oyvind.w.petersen@ntnu.no>
2024-03-12 19:49:55 +0100

    notation

--- models/cov_c2d.m
+++ models/cov_c2d.m
@@ -1,13 +1,12 @@
-function Qd=cov_c2d(Fac,Qc,dt,method)
+function Qd=cov_c2d(Ac,Qc,dt,method)
 
 %% Convert covariance from cont to disc 
 %
 % Model:
-% x(k+1)=F*x(k)+w(k);
-% y=H*x(k)+v(k);
+% x(k+1)=A*x(k)+w(k);
 %
 % Inputs:
-% Fac: state matrix (cont)
+% Ac: state matrix (cont)
 % Qc: covariance matrix (cont)
 % dt: time discretization (cont)
 % method: 'matrix' or 'integral', should give same result but the former is faster
@@ -22,7 +21,7 @@ if nargin==3
     method='matrix';
 end
 
-ns=size(Fac,1);
+ns=size(Ac,1);
 
 %% Matrix method
 
@@ -30,7 +29,7 @@ ns=size(Fac,1);
 
 if strcmpi(method,'matrix')
 
-	Matrix_1= expm( [Fac Qc ; zeros(ns) -Fac.']*dt);
+	Matrix_1= expm( [Ac Qc ; zeros(ns) -Ac.']*dt);
 	Matrix_2=[ zeros(ns) ; eye(ns) ];
 
 	Matrix_3=Matrix_1*Matrix_2;
@@ -46,7 +45,7 @@ end
 
 if strcmpi(method,'integral')
 
-	Psi_a_func = @(tau) expmnorm(Fac*tau);
+	Psi_a_func = @(tau) expmnorm(Ac*tau);
 
 	n=300;
 	tau_axis=linspace(0,dt,n);
@@ -87,10 +86,10 @@ Qd=forcesym(Qd);
 
 %%
 
-% Fac=magic(50); Fac=Fac+Fac.';
-% Fac=diag(-[0.2:0.2:2]); Fac=Fac-(magic(size(Fac))+magic(size(Fac)).')/1000;
+% Ac=magic(50); Ac=Ac+Ac.';
+% Ac=diag(-[0.2:0.2:2]); Ac=Ac-(magic(size(Ac))+magic(size(Ac)).')/1000;
 % 
-% Qc=eye(size(Fac)); a=randn(size(Qc)); a=(a+a.')/10; Qc=Qc+a;
+% Qc=eye(size(Ac)); a=randn(size(Qc)); a=(a+a.')/10; Qc=Qc+a;
 % dt=0.05
 % 
 % plotcorr(Qc)
