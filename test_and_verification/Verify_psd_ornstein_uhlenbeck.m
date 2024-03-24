198f74d39cb31f0b4ec02c458187806e3c61f530
Øyvind <oyvind.w.petersen@ntnu.no>
2024-03-24 10:09:31 +0100

    notation

--- test_and_verification/Verify_psd_ornstein_uhlenbeck.m
+++ test_and_verification/Verify_psd_ornstein_uhlenbeck.m
@@ -7,7 +7,7 @@ close all
 dt=0.02
 
 t=[0:dt:10000];
-w_axis=[0:0.001:1/dt/2]*2*pi;
+omega_axis=[0:0.001:1/dt/2]*2*pi;
 
 % Matern 0.5 
 lambda=0.35;
@@ -22,12 +22,12 @@ B_sim=eye(1);
 sigma_wc_squared=8^2
 sigma_wd_squared=Bc*sigma_wc_squared*Bc.'*dt;
 
-S_eta_cont(1,1,1:length(w_axis))=sigma_wc_squared/(2*pi);
-H_ss_cont=ssmod_tf(Ac,Bc,G,J,w_axis,[]);
+S_eta_cont(1,1,1:length(omega_axis))=sigma_wc_squared/(2*pi);
+H_ss_cont=ssmod_tf(Ac,Bc,G,J,omega_axis,[]);
 S_y_ss_cont=mtimes3(H_ss_cont,S_eta_cont,H_ss_cont,'nnh');
 
-H_ss_disc=ssmod_tf(A,B_sim,G,J,w_axis,dt);
-S_eta_disc(1,1,1:length(w_axis))=sigma_wd_squared/(2*pi); % Not quite sure why this is, but it gives correct
+H_ss_disc=ssmod_tf(A,B_sim,G,J,omega_axis,dt);
+S_eta_disc(1,1,1:length(omega_axis))=sigma_wd_squared/(2*pi); % Not quite sure why this is, but it gives correct
 S_y_ss_disc=mtimes3(H_ss_disc,S_eta_disc*dt,H_ss_disc,'nnh'); % Not quite sure why this is, but it gives correct
 
 % Simulate in time
@@ -36,7 +36,7 @@ w_sim=randn(size(t))*sigma_wd_squared.^0.5;
 plottime(t,y_sim)
 
 % From theory, see powerpoint
-S_theory(1,1,:)=(Bc*Gc)^2*sigma_wc_squared/(2*pi)./(lambda.^2+w_axis.^2);
+S_theory(1,1,:)=(Bc*Gc)^2*sigma_wc_squared/(2*pi)./(lambda.^2+omega_axis.^2);
 
 % Empirical from Welch
 [S_welch,w_welch]=estimateSpectrumWelch(y_sim,1/dt,'Nwelch',100,'unit','rad','plot','no');
@@ -45,6 +45,6 @@ plotopt=struct()
 plotopt.xlim=[0 100];
 plotopt.linestyle={'-' '--' ':' '--'}
 
-plotpsd(w_welch,S_welch,w_axis,S_y_ss_cont*2,w_axis,S_theory*2,plotopt);
+plotpsd(w_welch,S_welch,omega_axis,S_y_ss_cont*2,omega_axis,S_theory*2,plotopt);
 
 tilefigs([],'l');
