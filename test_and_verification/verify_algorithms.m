8a33f2677f341ca5c1e5d6e278c94905773a3b42
Øyvind <oyvind.w.petersen@ntnu.no>
2024-03-28 21:12:10 +0100

    verification alg

--- test_and_verification/verify_algorithms.m
+++ test_and_verification/verify_algorithms.m
@@ -9,11 +9,14 @@ close all
 mod=importbeamquick(6);
 
 mod.dt=0.01;
+% mod.dt=0.025/10;
 
 %% Disc force
 
 % mod.a_cell={'10_U' '40_U'  '80_U'  }
 % mod.d_cell={'15_U'}
+
+% mod.a_cell=genlabels('U',[10:10:90]); % For test modal expansion
 
 mod.a_cell={'10_U'  '50_U'  '80_U' }
 mod.d_cell={'15_U'  '55_U'}
@@ -23,14 +26,13 @@ mod.e_cell={'25_U' '45_U' '80_U'}
 [mod.Sd,mod.Sa,mod.Sp]=DofSelection(mod.d_cell,mod.a_cell,mod.p_cell,mod.doflabel);
 [~,~,mod.Se]=DofSelection({},{},mod.e_cell,mod.doflabel);
 
-[mod.A mod.B mod.G mod.J mod.Ac mod.Bc]=ssmod_modal(mod.phi,mod.Omega,mod.Gamma,mod.Sa,mod.Sd,mod.Sp,mod.dt,'force','disc');
+[mod.A mod.B mod.G mod.J mod.Ac mod.Bc mod.Gc mod.Jc]=ssmod_modal(mod.phi,mod.Omega,mod.Gamma,mod.Sa,mod.Sd,mod.Sp,mod.dt,'force','disc');
 [~, mod.Be ,~,mod.Je]=ssmod_modal(mod.phi,mod.Omega,mod.Gamma,mod.Sa,mod.Sd,mod.Se,mod.dt,'force','disc');
 
 mod.ny=size(mod.Sa,1); mod.nx=size(mod.A,1); mod.np=size(mod.Sp,2);
 
 %%
 
-Ce=[1 0.5 0 ; 0.5 0.5 0.5 ; 0 0.5 1]
 Ce=eye(3);
 mod.R0=eye(mod.ny)*[1e-4]^2;
 mod.Q0=eye(mod.nx)*[1e-8]^2;
@@ -38,8 +40,12 @@ mod.Q0=eye(mod.nx)*[1e-8]^2;
 mod.Q=mod.Be*Ce*mod.Be.'+mod.Q0;
 mod.R=mod.Je*Ce*mod.Je.'+mod.R0;
 mod.S=mod.Be*Ce*mod.Je.';
-% Ctot=[mod.Q mod.S ; mod.S.' mod.R]
-Ccorr=plotcorr([mod.Q mod.S ; mod.S.' mod.R]);
+
+mod.Q=mod.Q*100;
+mod.R=mod.R*100;
+mod.S=mod.S*100;
+
+plotcorr([mod.Q mod.S ; mod.S.' mod.R]);
 
 sim.nt=1e5+1;
 sim.t=[1:sim.nt]*mod.dt;
@@ -48,7 +54,7 @@ sim.t=[1:sim.nt]*mod.dt;
 
 close all
 % sim.p=BandLimitedWhiteNoise(0.1,3,sim.t,mod.np)*1;
-sim.p=bellshapednoise(0.1,1,sim.t,mod.np)*10;
+sim.p=bellshapednoise(0.05,1,sim.t,mod.np)*10;
 
 [sim.w,sim.v]=cov_noisegen(mod.Q,mod.R,mod.S,sim.t);
 
@@ -61,15 +67,27 @@ plottime(sim.t,sim.y);
 tilefigs
 
 return
-%% Estimate
+
+%% KF
+
+[x_k_k,x_k_kmin,P_k_k,P_k_kmin]=KF(mod.A,mod.B,mod.G,mod.J,mod.Q,mod.R,mod.S,sim.y,sim.p,[],[],'steadystate',true);
 
 close all
 
-% mod.P01=eye(size(mod.Q));
+plottime(sim.t,sim.x,x_k_kmin,x_k_k);
+plotfreq(sim.t,sim.x,x_k_kmin,x_k_k,'xlim',[0 10]);
+
+% plotstat(diag(P_k_kmin),diag(P_k_k),std(sim.x-x_k_kmin,0,2).^2,std(sim.x-x_k_k,0,2).^2);
+plotstat(std(sim.x-x_k_kmin,0,2).^2./diag(P_k_kmin),std(sim.x-x_k_k,0,2).^2./diag(P_k_k));
+
+
+%% JIS
+
+close all
+
 mod.P01=[];
 
 [x_jis_old p_jis_old Px_jis_ss_old Pp_jis_ss_old ] = JIS_ss_old(mod.A,mod.B,mod.G,mod.J,sim.y,sim.x0,mod.Q,mod.R,mod.S,mod.P01,'trunc',false);
-
 
 [x_jis p_jis Px_jis_ss Pp_jis_ss] = JIS_ss(mod.A,mod.B,mod.G,mod.J,sim.y,sim.x0,mod.Q,mod.R,mod.S,mod.P01,'trunc',false);
 
@@ -77,31 +95,15 @@ fid.L=10;
 [x_smooth p_smooth Px_smooth_ss Pp_smooth_ss ]=JIS_smooth(mod.A,mod.B,mod.G,mod.J,sim.y,mod.Q,mod.R,mod.S,sim.x0,mod.P01,fid.L,'convtol',1e-10);
 
 plottime(sim.t,sim.p,p_jis_old,p_jis,p_smooth,'displayname',{'True' 'Filt old' 'Filt' 'Smooth'});
-% 
-% plotfreq(sim.t,sim.p,p_jis,p_smooth,'xlim',[0 50],'displayname',{'True' 'Filt' 'Smooth'});
 
-% plottime(sim.t,sim.x,x_jis,x_smooth);
-
-% plotfreq(sim.t,sim.x(1:6,:),x_jis(1:6,:),x_smooth(1:6,:),'xlim',[0 50]);
-
-% tilefigs
-return
-
-% return
-%% Check statistics (best for long time series, band limited white noise input)
-
-close all
+% close all
 
 plotopt=struct();
 plotopt.gap=[0.1 0.1];
 plotopt.marg_h=[0.1 0.25];
 plotopt.ylabel='Error SD';
-plotopt.displayname={'JIS' 'Smooth' };
-plotopt.displayname2={'JIS' 'Smooth' };
 plotopt.markersize=10;
 plotopt.color={[0 0 1] [1 0 0]   [0 0 1] [1 0 0]  };
-% plotopt.marker={ 'x' 'x' 'x' 'x'  };
-% plotopt.xticklabel={'x_1' 'x_2' 'x_3' 'x_4' 'x_5' 'x_6'  'x_7' 'x_8' 'x_9' 'x_{10}' 'p'};
 % plotopt.split={};
 
 % States
@@ -112,21 +114,22 @@ plot_std_pred_x={};
 plot_std_pred_x{1}=[diag(Px_jis_ss).^0.5 ];
 plot_std_pred_x{2}=[diag(Px_smooth_ss).^0.5 ];
 
-plotstatstem(plot_std_emp_x,plot_std_pred_x,plotopt);
+plotstat(plot_std_emp_x{1}./plot_std_pred_x{1},plot_std_emp_x{2}./plot_std_pred_x{2},plotopt);
 
-legendadjust('east',[-0.1 0.05],4,12);
-axistight(gcf,[0.3 1],'ylog','keepx');
+% legendadjust('east',[-0.1 0.05],4,12);
+% axistight(gcf,[0.3 1],'ylog','keepx');
 
 % Force
 plot_std_emp_p=sdstatemp(sim.p,{[p_jis] , [p_smooth]},[50 50])
 
-plot_std_pred_p={};
 plot_std_pred_p{1}=[diag(Pp_jis_ss).^0.5];
 plot_std_pred_p{2}=[diag(Pp_smooth_ss).^0.5];
 
-plotstatstem(plot_std_emp_p,plot_std_pred_p,plotopt);
+plotstat(plot_std_emp_p{1},plot_std_pred_p{1},plotopt);
 
-return
+plotstat(plot_std_emp_p{2},plot_std_pred_p{2},plotopt);
+
+tilefigs
 
 %% Test lag
 
@@ -147,35 +150,11 @@ figure(); hold on; grid on;
 plot(L_mat,Pp_all); ylog;
 
 
-%%
+%% KF
 
-[x_k_k,x_k_kmin,x_k_N,P_k_k,P_k_kmin,P_k_N]=KF_RTS(mod.A,mod.B,mod.G,mod.J,mod.Q,mod.R,mod.S,sim.y,sim.p,[],[]);
+[x_k_k,x_k_kmin,x_k_N,P_k_k,P_k_kmin,P_k_N]=KF_RTS(mod.A,mod.B,mod.G,mod.J,mod.Q,mod.R,mod.S,sim.y,sim.p);
 
-% P_0_0=[]
-% 
-% [x_k_k,x_k_kmin,P_k_k,P_k_kmin,K_k_ss]=KF(mod.A,mod.B,mod.G,mod.J,mod.Q,mod.R,mod.S,sim.y,sim.p,sim.x0,P_0_0);
-% 
-% % For RTS, S must be handled by decoupling and new system matrices
-% 
-% mod.A_star=mod.A-mod.S/mod.R*mod.G;
-% mod.B_star=[mod.B-mod.S/mod.R*mod.J mod.S/mod.R];
-% mod.G_star=[mod.G ];
-% mod.J_star=[mod.J zeros(mod.ny,mod.ny)];
-% 
-% sim.p_star=[sim.p ; sim.y];
-% mod.Q_star=mod.Q-mod.S/mod.R*mod.S.';
-% mod.S_star=zeros(size(mod.A,1),size(mod.G,1));
-% mod.R_star=mod.R;
-% 
-% [x_k_k_tr,x_k_kmin_tr,P_k_k_tr,P_k_kmin_tr]=KF(mod.A_star,mod.B_star,mod.G_star,mod.J_star,mod.Q_star,mod.R_star,mod.S_star,sim.y,sim.p_star,sim.x0,P_0_0);
-% 
-% close all
-% plottime(sim.t,x_k_k,x_k_k_tr)
-% plottime(sim.t,x_k_kmin,x_k_kmin_tr)
-% 
-% [x_k_N_tr,P_k_N_tr]=RTSSmoother(mod.A_star,x_k_k_tr,x_k_kmin_tr,P_k_k_tr,P_k_kmin_tr);
-
-%% Check statistics (best for long time series, band limited white noise input)
+% Check statistics (best for long time series, band limited white noise input)
 
 close all
 
@@ -184,10 +163,9 @@ plotopt.gap=[0.1 0.1];
 plotopt.marg_h=[0.1 0.25];
 plotopt.ylabel='Error SD';
 plotopt.displayname={'Pred' 'Filt' 'Smooth' };
-plotopt.displayname2={'Pred' 'Filt' 'Smooth' };
 plotopt.markersize=10;
-plotopt.color={[0 0 1] [1 0 0] [0 0 0]   [0 0 1] [1 0 0]  [0 0 0]};
-plotopt.marker={ 'x' 'x' 'x' 'x'  };
+plotopt.color={[0 0 1] [1 0 0] [0 0 0]  };
+plotopt.marker={ 'x' 'o' 'd'};
 
 % States
 
@@ -198,9 +176,11 @@ plot_std_pred_x{1}=[diag(P_k_kmin).^0.5];
 plot_std_pred_x{2}=[diag(P_k_k).^0.5];
 plot_std_pred_x{3}=[diag(P_k_N).^0.5];
 
-plotstatstem(plot_std_emp_x,plot_std_pred_x,plotopt);
+plotstat(plot_std_emp_x{1},plot_std_pred_x{1},plotopt);
+plotstat(plot_std_emp_x{2},plot_std_pred_x{2},plotopt);
+plotstat(plot_std_emp_x{3},plot_std_pred_x{3},plotopt);
 
-legendadjust('east',[-0.1 0.05],4,12);
-axistight(gcf,[0.3 1],'ylog','keepx');
+plotopt.normalize=true;
+plotstat(plot_std_emp_x{:},plotopt);
 
-return
+tilefigs
